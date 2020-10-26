4cf5b21ba5c693f9d13830ba8438f92c9029d9ad
Shreyas Kalvankar <shreyaskalvankar@gmail.com>
2020-10-26 07:55:14 +0530

    Initial Commit

---
+++ Training_data.py
@@ -0,0 +1,46 @@
+import os
+from PIL import Image, ImageOps
+import numpy as np
+
+IMG_SIZE = 720
+IMG_CHANNELS = 1
+DIR = 'Data/'
+
+cntr = 0
+image_list = os.listdir(DIR)
+while(cntr<4):
+    training_data = []
+    print(f'Reading batch {cntr}')
+    for images in image_list[cntr*5000:(cntr+1)*5000]:
+        path = os.path.join(DIR,images)
+        image = Image.open(path).resize((IMG_SIZE,IMG_SIZE), Image.ANTIALIAS)
+        image = ImageOps.grayscale(image)
+        training_data.append(np.array(image))
+
+    print(f'Resizing batch {cntr}')
+    training_data = np.reshape(
+        training_data, (-1, IMG_SIZE, IMG_SIZE, IMG_CHANNELS))
+
+    training_data = training_data / 127.5 - 1
+
+    print(f'Saving batch {cntr}')
+    np.save(f'training/sunset_greyscale_batch_{cntr}.npy', training_data)
+    del training_data[:]
+    cntr+=1
+
+training_data = []
+print(f'Reading batch {cntr}')
+for images in image_list[cntr*5000:]:
+    path = os.path.join(DIR,images)
+    image = Image.open(path).resize((IMG_SIZE,IMG_SIZE), Image.ANTIALIAS)
+    image = ImageOps.grayscale(image)
+    training_data.append(np.array(image))
+
+print(f'Resizing batch {cntr}')
+training_data = np.reshape(
+    training_data, (-1, IMG_SIZE, IMG_SIZE, IMG_CHANNELS))
+
+training_data = training_data / 127.5 - 1
+
+print(f'Saving batch {cntr}')
+np.save(f'training/sunset_greyscale_batch_{cntr}.npy', training_data)
